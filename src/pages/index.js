d1dd96007e9da989a88a033e0934d632910bdf6b
Mohammad Abubaker <Mohammad_Abubaker@yahoo.com>
2020-09-04 13:23:02 +0500

    First commet from WebStorm.

--- src/pages/index.js
+++ src/pages/index.js
@@ -7,14 +7,16 @@ import SEO from "../components/seo"
 
 const IndexPage = () => (
   <Layout>
-    <SEO title="Home" />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
+    <SEO title="Home"/>
+    <h1>Learn to Design and Code React apps.</h1>
+    <p>Complete courses about the best tools and design systems. Prototype and build apps with React and Swift.</p>
+
+    <!-- First Comment -->
     <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-      <Image />
+      <Image/>
     </div>
-    <Link to="/page-2/">Go to page 2</Link> <br />
+
+    <Link to="/page-2/">Go to page 2</Link> <br/>
     <Link to="/using-typescript/">Go to "Using TypeScript"</Link>
   </Layout>
 )
